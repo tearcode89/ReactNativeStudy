2d5f8bacaddd4e1625f9b6e7b50788c9f607ec9a
tearcode89 <sjun8932.3@gmail.com>
2023-07-15 14:41:16 +0900

    2023-07-15 챕터 ch02_3_84Page 까지 진행

--- ch02/ch02_3/src/screens/ClassComponent.tsx
+++ ch02/ch02_3/src/screens/ClassComponent.tsx
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
+import {Text} from 'react-native';
+import * as D from '../data';
 
-export default class ClassComponent extends Component<Component, any> {
-  render() {
-    return null;
+const person = D.createRandomPerson();
+export default class ClassComponent extends Component {
+  render(): React.JSX.Element {
+    return <Text>{JSON.stringify(person, null, 2)}</Text>;
   }
 }
