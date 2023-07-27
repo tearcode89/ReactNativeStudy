fe707d81111763f3a704f79b6b7d5c32c3410bf0
tearcode89 <sjun8932.3@gmail.com>
2023-07-27 15:35:15 +0900

    2023-07-27 챕터 ch05_2_327Page 까지 진행

---
+++ ch05/ch05_2/src/screens/Home.tsx
@@ -0,0 +1,42 @@
+import React, {useState, useCallback} from "react";
+import {StyleSheet, View, Text, Switch, FlatList} from 'react-native';
+import {useToggleTheme} from "../contexts";
+import * as D from '../data'
+import Person from './Person';
+import {useTheme} from "react-native-paper";
+
+export default function People() {
+    const [people, setPeople] = useState<D.IPerson[]>([D.createRandomPerson()])
+    const theme = useTheme()
+    const toggleTheme = useToggleTheme()
+    const add = useCallback(() => {
+        setPeople((people) => [...people, D.createRandomPerson()])
+    }, [])
+    const removeAll = useCallback(() => {
+        setPeople((notUsed) => [])
+    }, [])
+    return (
+        <View style={[styles.view, {backgroundColor: theme.colors.surface}]}>
+            <View style={[styles.topBar, {backgroundColor: theme.colors.accent}]}>
+                <Text>
+
+                </Text>
+                <Text>
+
+                </Text>
+                <View/>
+                <Switch/>
+            </View>
+            <FlatList
+                data={people}
+                renderItem={({item}) => <Person person={item}/>}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    view: {flex: 1},
+    topBar: {flexDirection: 'row', padding: 5},
+    text: {marginRight: 10, fontSize: 20}
+})
